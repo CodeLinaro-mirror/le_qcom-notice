229571b91fc49abe1fc096aef8e64d4ff6f9d6ab
Linux Build Service Account <lnxbuild@localhost>
2026-06-25 05:11:41 -0700

    Update commands to run bitbake package-index
    Change-Id: Iebee5020b8ac7e2d1e2a405d06b41d507d113717

--- sync_build_kas.sh
+++ sync_build_kas.sh
@@ -95,7 +95,7 @@ if [[ "$ARCH" =~ "arm" ]]; then
 fi
 
 if [[ "$ARCH" =~ "arm" ]]; then
-   DISTRO="qcom-distro"
+   DISTRO="qcom-distro-sota"
    echo "Architecture is $ARCH: Compile for Generic target, compile eSDK and standard SDK for generic target, distro=$DISTRO"
    time kas build meta-qcom/ci/qcom-armv8a.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/mirror-tarballs.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
    sleep 3
@@ -108,22 +108,29 @@ else
       sleep 3
       kas shell meta-qcom/ci/qcom-armv8a.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/mirror-tarballs.yml:meta-qcom/ci/linux-qcom-6.18.yml -c "bitbake -c populate_sdk qcom-multimedia-proprietary-image && bitbake -c populate_sdk_ext qcom-multimedia-proprietary-image"
    else
-      DISTRO="qcom-distro"
+      DISTRO="qcom-distro-sota"
       echo "Architecture is $ARCH: Compile for all applicable targets (KLMT), compile eSDK and standard SDK for generic target, distro=$DISTRO"
       # Run build
       time kas build meta-qcom/ci/rb3gen2-core-kit.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
+      time kas shell meta-qcom/ci/rb3gen2-core-kit.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake package-index"
       sleep 3
       time kas build meta-qcom/ci/iq-8275-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
+      time kas shell meta-qcom/ci/iq-8275-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake package-index"
       sleep 3
       time kas build meta-qcom/ci/iq-9075-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
+      time kas shell meta-qcom/ci/iq-9075-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake package-index"
       sleep 3
       time kas build meta-qcom/ci/iq-615-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
+      time kas shell meta-qcom/ci/iq-615-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake package-index"
       sleep 3
       time kas build meta-qcom/ci/iq-x7181-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
+      time kas shell meta-qcom/ci/iq-x7181-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake package-index"
       sleep 3
       time kas build meta-qcom/ci/iq-x5121-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
+      time kas shell meta-qcom/ci/iq-x5121-evk.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake package-index"
       sleep 3
       time kas build meta-qcom/ci/qcom-armv8a.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/mirror-tarballs.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml
+      time kas shell meta-qcom/ci/qcom-armv8a.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/mirror-tarballs.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake package-index"
       sleep 3
       kas shell meta-qcom/ci/qcom-armv8a.yml:meta-qcom/ci/${DISTRO}.yml:meta-qcom/ci/mirror-tarballs.yml:meta-qcom/ci/linux-qcom-6.18.yml:meta-qcom/ci/performance.yml -c "bitbake -c populate_sdk qcom-multimedia-proprietary-image && bitbake -c populate_sdk_ext qcom-multimedia-proprietary-image"
    fi
